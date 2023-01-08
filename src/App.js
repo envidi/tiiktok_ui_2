42fc4919a80cfeb79e785cae314fd293bf554fe2
duc <phanm711996@gmail.com>
2023-01-08 09:45:18 +0700

    Route/Layout 2 Tiktok

--- src/App.js
+++ src/App.js
@@ -9,14 +9,14 @@ function App() {
             <div className="App">
                 <Routes>
                     {publicRoutes.map((route, index) => {
+                        console.log(route);
                         const Page = route.component;
                         // const Layout = route.layout === null ? Fragment : DefaultLayout;
                         let Layout = DefaultLayout;
-                        if(route.layout ){
-                            Layout = route.layout
-                        }
-                        else if(route.layout === null){
-                            Layout = Fragment
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
                         }
                         return (
                             <Route
